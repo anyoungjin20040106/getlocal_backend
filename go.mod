b3d7edb433b46023819064198fab7a3ee972cca4
안영진 <158057603+janyoungjin@users.noreply.github.com>
2024-06-04 13:29:53 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,36 @@
+module GetLocal_backend
+
+go 1.22.2
+
+require github.com/rocketlaunchr/dataframe-go v0.0.0-20211025052708-a1030444159b
+
+require (
+	github.com/apache/arrow/go/arrow v0.0.0-20211112161151-bc219186db40 // indirect
+	github.com/apache/thrift v0.20.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
+	github.com/gonum/lapack v0.0.0-20181123203213-e4cdc5a0bff9 // indirect
+	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/guptarohit/asciigraph v0.7.1 // indirect
+	github.com/juju/clock v1.0.3 // indirect
+	github.com/juju/errors v1.0.0 // indirect
+	github.com/juju/loggo v1.0.0 // indirect
+	github.com/juju/utils/v2 v2.0.0-20210305225158-eedbe7b6b3e2 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/rocketlaunchr/mysql-go v1.1.3 // indirect
+	github.com/sjwhitworth/golearn v0.0.0-20221228163002-74ae077eafb2 // indirect
+	github.com/xitongsys/parquet-go v1.6.2 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/exp v0.0.0-20240531132922-fd00a4e0eefc // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
+	gonum.org/v1/gonum v0.9.3 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/rocketlaunchr/dataframe-go"
+	"github.com/rocketlaunchr/dataframe-go/imports"
+	"github.com/sjwhitworth/golearn/base"
+	"github.com/sjwhitworth/golearn/knn"
+)
+
+type LoacalData struct {
+	local string
+}
+
+func MakeDataFrame(url string) *dataframe.DataFrame {
+	reps, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	data, err := ioutil.ReadAll(reps.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer reps.Body.Close()
+	reader := bytes.NewReader(data)
+	result, err := imports.LoadFromCSV(context.Background(), reader)
+	if err != nil {
+		panic(err)
+	}
+	for _, i := range [2]int{1, 2} {
+		fSeries, err := result.Series[i].(dataframe.ToSeriesFloat64).ToSeriesFloat64(context.Background(), true)
+		if err != nil {
+			panic(err)
+		}
+		result.Series[i] = fSeries
+	}
+	return result
+}
+func main() {
+	df := MakeDataFrame("https://raw.githubusercontent.com/janyoungjin/localData/main/data.csv")
+	instances := base.ConvertDataFrameToInstances(df, 0)
+	cls := knn.NewKnnClassifier("euclidean", "linear", 2)
+	cls.AllowOptimisations = false
+	cls.Fit(instances)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		xVal := r.PostFormValue("x")
+		yVal := r.PostFormValue("y")
+
+		x, err := strconv.ParseFloat(xVal, 64)
+		if err != nil {
+			http.Error(w, "Invalid value for x", http.StatusBadRequest)
+			return
+		}
+
+		y, err := strconv.ParseFloat(yVal, 64)
+		if err != nil {
+			http.Error(w, "Invalid value for y", http.StatusBadRequest)
+			return
+		}
+
+		vec := base.NewDenseInstances()
+		ax := base.NewFloatAttribute("x")
+		ay := base.NewFloatAttribute("y")
+		vec.AddAttribute(ax)
+		vec.AddAttribute(ay)
+		axSpec := vec.AddAttribute(ax)
+		aySpec := vec.AddAttribute(ay)
+		vec.AddClassAttribute(base.NewCategoricalAttribute())
+		vec.Extend(1)
+		vec.Set(axSpec, 0, ax.GetSysValFromString(fmt.Sprintf("%f", x)))
+		vec.Set(aySpec, 0, ay.GetSysValFromString(fmt.Sprintf("%f", y)))
+
+		predictions, err := cls.Predict(vec)
+		if err != nil {
+			panic(err)
+		}
+		result := LoacalData{
+			local: predictions.RowString(0),
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(result)
+
+	})
+	http.ListenAndServe(":8000", nil)
+}
